7bd8d3bc1c35e529680691660a8dc7a8c814977d
Kyaw Ko Lwin <kyawkolwin87@gmail.com>
2024-12-02 17:11:36 +0700

    Add project files.

---
+++ frmoldMobilekeypad.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace IronSoftware_Challenge
+{
+    partial class frmoldMobilekeypad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btnstar = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDisplay.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplay.ForeColor = System.Drawing.Color.IndianRed;
+            this.lblDisplay.Location = new System.Drawing.Point(12, 9);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(495, 216);
+            this.lblDisplay.TabIndex = 0;
+            this.lblDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(12, 267);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(150, 69);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "1\r\n& (";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(186, 267);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(150, 69);
+            this.btn2.TabIndex = 3;
+            this.btn2.Text = "2 \r\na b c\r\n";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(357, 267);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(150, 69);
+            this.btn3.TabIndex = 4;
+            this.btn3.Text = "3\r\nd e f";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(12, 356);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(150, 69);
+            this.btn4.TabIndex = 5;
+            this.btn4.Text = "4\r\ng h i";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(186, 356);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(150, 69);
+            this.btn5.TabIndex = 6;
+            this.btn5.Text = "5\r\nj k l";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(357, 356);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(150, 69);
+            this.btn6.TabIndex = 7;
+            this.btn6.Text = "6\r\nm n o\r\n";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(12, 443);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(150, 69);
+            this.btn7.TabIndex = 8;
+            this.btn7.Text = "7 \r\np q r s";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(186, 443);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(150, 69);
+            this.btn8.TabIndex = 9;
+            this.btn8.Text = "8\r\nt u v";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(357, 443);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(150, 69);
+            this.btn9.TabIndex = 10;
+            this.btn9.Text = "9\r\nw x y z";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnstar
+            // 
+            this.btnstar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnstar.Location = new System.Drawing.Point(12, 535);
+            this.btnstar.Name = "btnstar";
+            this.btnstar.Size = new System.Drawing.Size(150, 69);
+            this.btnstar.TabIndex = 11;
+            this.btnstar.Text = "* <x";
+            this.btnstar.UseVisualStyleBackColor = true;
+            this.btnstar.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn0.Location = new System.Drawing.Point(186, 535);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(150, 69);
+            this.btn0.TabIndex = 12;
+            this.btn0.Text = "0 \r\nspace";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnter.Location = new System.Drawing.Point(357, 535);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(150, 69);
+            this.btnEnter.TabIndex = 13;
+            this.btnEnter.Text = "#\r\n>\r\n";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(12, 610);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(495, 31);
+            this.btnClear.TabIndex = 14;
+            this.btnClear.Text = "Clear Display";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // frmoldMobilekeypad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 651);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btnstar);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lblDisplay);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmoldMobilekeypad";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btnstar;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ frmoldMobilekeypad.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IronSoftware_Challenge
+{
+    public partial class frmoldMobilekeypad : Form
+    {
+        // Creating character array align with keypad number
+        char[][] keyPadCharacter =
+        {
+            new char[] { '&', '!', '('},
+            new char[] { 'a', 'b', 'c'},
+            new char[] { 'd', 'e', 'f'},
+            new char[] { 'g', 'h', 'i'},
+            new char[] { 'j', 'k', 'l'},
+            new char[] { 'm', 'n', 'o'},
+            new char[] { 'p', 'q', 'r', 's' },
+            new char[] { 't', 'u', 'v'},
+            new char[] { 'w', 'x', 'y','z'},
+            new char[] { '*'},
+            new char[] {' '},
+            new char[] {'#'},
+
+        };
+        string sInputNumber="", sCurrentNumber = "", sPreviousNumber ="", sAllnumber = "";
+        string sResult = "";
+
+        public frmoldMobilekeypad()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e) //Button Click Event For 1 - 9
+        {
+            if (sender.ToString() == "System.Windows.Forms.Button, Text: 1\r\n& (") // Assign Button Number Into Variable
+            {
+                sInputNumber += "1"; // Assign Button Number 
+            }
+
+            if (sender.ToString() == "System.Windows.Forms.Button, Text: 2 \r\na b c\r\n")
+            {
+                sInputNumber += "2";
+            }
+            if (sender.ToString() == "System.Windows.Forms.Button, Text: 3\r\nd e f")
+            {
+                sInputNumber += "3";
+            }
+            if (sender.ToString() == "System.Windows.Forms.Button, Text: 4\r\ng h i")
+            {
+                sInputNumber += "4";
+            }
+            if (sender.ToString() == "System.Windows.Forms.Button, Text: 5\r\nj k l")
+            {
+                sInputNumber += "5";
+            }
+            if (sender.ToString() == "System.Windows.Forms.Button, Text: 6\r\nm n o\r\n")
+            {
+                sInputNumber += "6";
+            }
+            if (sender.ToString() == "System.Windows.Forms.Button, Text: 7 \r\np q r s")
+            {
+                sInputNumber += "7";
+            }
+            if (sender.ToString() == "System.Windows.Forms.Button, Text: 8\r\nt u v")
+            {
+                sInputNumber += "8";
+            }
+            if (sender.ToString() == "System.Windows.Forms.Button, Text: 9\r\nw x y z")
+            {
+                sInputNumber += "9";
+            }
+            if (sender.ToString() == "System.Windows.Forms.Button, Text: 0 \r\nspace")
+            {
+                sInputNumber += "0";
+            }
+            lblDisplay.Text = sInputNumber;
+          
+        }
+
+        private string oldPhonePad(string sInput) 
+        {
+            string sTextResult = "";
+            int iSameNumberCount = 0; 
+
+            for (int i = 0; i < sInput.Length; i++) // Loop the whole number sentence
+            {
+                sCurrentNumber = sInput[i].ToString();
+                if (sCurrentNumber == "0") //Check if there is space
+                {
+                    sPreviousNumber = "";
+                    iSameNumberCount = 0;
+                }
+                else
+                {
+                    if (sCurrentNumber == sPreviousNumber) // If current number is same with previous number, system will pick next character depend on total number of Number
+                    {
+                        timer1.Stop();
+                        timer1.Start();
+
+                        iSameNumberCount++;
+
+                        if (sTextResult.Length > 0)
+                        {
+                            sTextResult = sTextResult.Remove(sTextResult.Length - 1);
+                        }
+
+                        sTextResult += keyPadCharacter[Convert.ToInt32(sCurrentNumber) - 1][iSameNumberCount].ToString();
+                    }
+                    else
+                    {
+                        sTextResult += keyPadCharacter[Convert.ToInt32(sCurrentNumber) - 1][iSameNumberCount].ToString();
+                        sPreviousNumber = sCurrentNumber;
+                        iSameNumberCount = 0;
+                    }
+                }
+            }
+           
+
+            return sTextResult;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "";
+            sCurrentNumber = "";
+            sPreviousNumber = "";
+            sInputNumber = "";
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text =  oldPhonePad(lblDisplay.Text); //Call function to convert digit number into character and show it in display
+        }
+        private void btnDelete_Click(object sender, EventArgs e) // Press < button to delete character
+        {
+            string sDisplayText = lblDisplay.Text;
+            if (sDisplayText.Length > 0)
+            {
+                sDisplayText = sDisplayText.Remove(sDisplayText.Length - 1);
+                lblDisplay.Text = sDisplayText;
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            sPreviousNumber = "";
+            sResult = "";
+        }
+
+    }
+}
